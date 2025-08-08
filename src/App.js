12aeb3d37d7531b5b612115582ec52f612ee082d
swapnshweta <pawar.swapnil59@gmail.com>
2025-08-08 07:31:54 +0530

    Initial React Appqqs

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import First from './Component/first';
+import Click from './Component/Click';
+import Count from './Component/Count';
+import DemoEffect from './Component/DemoEffect';
+import HookCounterOne from './Component/HookCounterOne';
+import Try from './Component/Try';
+import C1 from './Component/C1';
+
+function App() {
+  return (
+    
+    <div className="App">
+      <h1 className="">Welcome to react</h1>
+        <Try name="Swapnil"/>
+        <C1 age="20"/>
+
+      
+    </div>
+    
+    
+  );
+}
+
+export default App;
